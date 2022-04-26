4613f513eaf8af922642d2cf0467679bceeab3e4
JoseFranciscoPeraltaRamos <49628919+JFrankgod@users.noreply.github.com>
2022-04-26 02:30:52 -0500

    Agregando tipado a la peticion http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,7 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   private _historial: string[] = [];
   private apiKey: string = 'http://api.giphy.com/v1/gifs/search?api_key=X28JSEKvTKP90xuBwfspk9228DDmGup7&q=goku&limit=10'
-  public resultados : any[] =[];
+  public resultados : Gif[] =[];
 
   get historial(){
 
@@ -27,8 +28,8 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
     
-    this.http.get(`http://api.giphy.com/v1/gifs/search?api_key=X28JSEKvTKP90xuBwfspk9228DDmGup7&q=${query}&limit=10`)
-    .subscribe((resp: any) => {
+    this.http.get<SearchGifsResponse>(`http://api.giphy.com/v1/gifs/search?api_key=X28JSEKvTKP90xuBwfspk9228DDmGup7&q=${query}&limit=10`)
+    .subscribe((resp) => {
       console.log(resp.data);
       this.resultados = resp.data;
     } )
